49a310fbc83a76bf7ba89f1830ac9c2bd31deaca
ToMMaaY <thuynh629@gmail.com>
2019-01-11 13:27:38 -0800

    Fixed bug in projectModel.js

--- data/helpers/projectModel.js
+++ data/helpers/projectModel.js
@@ -1,20 +1,25 @@
-const db = require('../dbConfig.js');
-const mappers = require('./mappers');
+const db = require("../dbConfig.js");
+const mappers = require("./mappers");
 
 module.exports = {
   get: function(id) {
-    let query = db('projects as p');
+    let query = db("projects as p");
 
     if (id) {
-      query.where('p.id', id).first();
+      query.where("p.id", id).first();
 
       const promises = [query, this.getProjectActions(id)]; // [ projects, actions ]
 
       return Promise.all(promises).then(function(results) {
         let [project, actions] = results;
-        project.actions = actions;
 
-        return mappers.projectToBody(project);
+        if (!project) {
+          return null;
+        } else {
+          project.actions = actions;
+
+          return mappers.projectToBody(project);
+        }
       });
     }
 
@@ -23,24 +28,24 @@ module.exports = {
     });
   },
   getProjectActions: function(projectId) {
-    return db('actions')
-      .where('project_id', projectId)
+    return db("actions")
+      .where("project_id", projectId)
       .then(actions => actions.map(action => mappers.actionToBody(action)));
   },
   insert: function(project) {
-    return db('projects')
+    return db("projects")
       .insert(project)
       .then(([id]) => this.get(id));
   },
   update: function(id, changes) {
-    return db('projects')
-      .where('id', id)
+    return db("projects")
+      .where("id", id)
       .update(changes)
       .then(count => (count > 0 ? this.get(id) : null));
   },
   remove: function(id) {
-    return db('projects')
-      .where('id', id)
+    return db("projects")
+      .where("id", id)
       .del();
-  },
+  }
 };
